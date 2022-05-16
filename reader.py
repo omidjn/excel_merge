bcb09e83b59bfaa8dc198f14b7a1eb80bd57f905
dimo <omidjalilian147@gmail.com>
2022-05-16 15:48:54 +0430

    progress bar

--- reader.py
+++ reader.py
@@ -7,6 +7,27 @@ all_data = []
 
 facnum = 1
 
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 def read(loc, factor, n):
     global facnum
     wb_obj = openpyxl.load_workbook(loc)
@@ -55,9 +76,12 @@ factor_nums = read_main(dir + 'main.xlsx')
 all_lenght = len(files)
 n = 1
 
+printProgressBar(0, all_lenght, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 for i in files:
     if i.endswith(".xlsx"):
-        print(n, "/", all_lenght)
+        # print(n, "/", all_lenght)
+        printProgressBar(n + 1, all_lenght, prefix = 'Progress:', suffix = 'Complete', length = 50)
         n += 1
         try:
             d = read(os.path.join(file_dir, i), factor_nums, n)
